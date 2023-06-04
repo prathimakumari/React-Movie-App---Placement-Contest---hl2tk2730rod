4309b4afd538a63ac609574351987d16fdc32ee2
prathimakumari <115368988+prathimakumari@users.noreply.github.com>
2023-06-04 15:30:15 +0530

    Update Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,6 +1,6 @@
-const API_KEY = "31d362638d8091422fd6cc6d9503d706";
+const API_KEY = "64cb9bc3cfa735142d5b56af66f8d74a";
 const API_READ_ACCESS_TOKEN =
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMWQzNjI2MzhkODA5MTQyMmZkNmNjNmQ5NTAzZDcwNiIsInN1YiI6IjYzZDZiMjAxMjJkZjJlMDA3OTc2YTdlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IqzOwyHfqfUHUcTe0psECNZ-J0o9LLk3QacjL3z4sgY";
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGNiOWJjM2NmYTczNTE0MmQ1YjU2YWY2NmY4ZDc0YSIsInN1YiI6IjY0MjkzMDA4YTNlNGJhMDExMTRhNTEyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nT30W3A3RzQlqlDw_uGS6J7XtaKU32IKwVxNxyQ4QtA";
 
 export const searchMovieData = async (movieName) => {
     const query = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movieName}`;
